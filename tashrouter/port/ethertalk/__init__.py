dad2a03c2567813c9880dc34601a46388d2f4327
Micheal Waltz <micheal@ecliptik.com>
2025-04-14 15:16:30 -0700

    Race Condition in EtherTalkPort Address Mapping

--- tashrouter/port/ethertalk/__init__.py
+++ tashrouter/port/ethertalk/__init__.py
@@ -150,15 +150,17 @@ class EtherTalkPort(Port):
                                                                      0, network, node))))
   
   def _add_address_mapping(self, network, node, mapped_hw_addr):
-    '''Add an address mapping for the given network, node, and Ethernet address and release any held Datagrams waiting on it.'''
-    datagrams_to_send = deque()
     with self._tables_lock:
       self._address_mapping_table[(network, node)] = (mapped_hw_addr, time.monotonic())
       if (network, node) in self._held_datagrams:
-        for datagram, _ in self._held_datagrams[(network, node)]: datagrams_to_send.append((mapped_hw_addr, datagram))
+        datagrams_to_send = [(mapped_hw_addr, datagram) for datagram, _ in self._held_datagrams[(network, node)]]
         self._held_datagrams.pop((network, node))
-    for hw_addr, datagram in datagrams_to_send: self._send_datagram(hw_addr, datagram)
-  
+      else:
+        datagrams_to_send = []
+
+    for hw_addr, datagram in datagrams_to_send:
+        self._send_datagram(hw_addr, datagram)
+
   def _send_aarp_requests_run(self):
     '''Thread for sending AARP requests for held Datagrams.'''
     self._send_aarp_requests_started_event.set()
